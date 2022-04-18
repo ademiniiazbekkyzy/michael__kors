a68c2ad9d0d9c47568de2598cf16b499526e94b2
ademiniiazbekkyzy <ademi.niiazbekkyzy@mail.ru>
2022-04-18 19:32:02 +0600

    added models

--- product/urls.py
+++ product/urls.py
@@ -1,8 +1,8 @@
-from django.urls import path
-
-from product.views import CategoryListCreateView
-
-urlpatterrns = [
-    path('category/', CategoryListCreateView.as_view()),
-    path('category/<str:slug>', CategoryRetrieveDeleteUpdateView.as_view()),
-]
+# from django.urls import path
+#
+# from product.views import CategoryListCreateView
+#
+# urlpatterrns = [
+#     path('category/', CategoryListCreateView.as_view()),
+#     path('category/<str:slug>', CategoryRetrieveDeleteUpdateView.as_view()),
+# ]
